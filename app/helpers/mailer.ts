855b368bbd6fb5f5b33c7f99e005cb0503ce555c
gargeekadam09 <149878751+gargeekadam09@users.noreply.github.com>
2025-04-03 12:10:52 -1200

    Sign up and mail

---
+++ app/helpers/mailer.ts
@@ -0,0 +1,42 @@
+import nodemailer from 'nodemailer'
+import User from '../models/usermodel.mjs';
+import bcrypt from 'bcryptjs' 
+
+export const sendEmail = async({email, emailType, userId} :any) =>
+{
+    try{
+       const hashedToken = await bcryptjs.hash(userId.toString(), 10)
+
+        if(emailType ==="VERIFY"){
+            await User.findByIdAndUpdate(userId,
+                {verifyToken: hashedToken, verifyTokenExpiry: Date.now() + 3600000})
+
+        }else if (emailType === "RESET"){
+            await User.findByIdAndUpdate(userId,
+                {forgotPasswordToken: hashedToken,
+                 verifyPasswordTokenExpiry: Date.now() + 3600000})
+        }
+
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, // true for port 465, false for other ports
+            auth: {
+              user: "maddison53@ethereal.email",
+              pass: "jn7jnAPss4f63QBp6D",
+            },
+          });
+
+          const mailOptions = {
+            from: 'gargeekadam09@gmail.com', 
+            to: email,
+            subject: emailType === 'VERIFY' ? "Verify Your email" : "Reset your password",}
+
+          const mailResponse =  await transporter.sendMail(mailOptions)
+          return mailResponse
+
+
+    }catch(error:any){
+          throw new Error (error.message)
+    }
+}
