4b678f330755b2dc9ebbc02166848b64c383ff05
gargeekadam09 <149878751+gargeekadam09@users.noreply.github.com>
2025-04-05 00:32:48 -1200

    DB Configuration

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {connect} from '@/dbConfig/dbConfig'
+import User from '@/models/userModel'
+import {NextRequest , NextResponse } from 'next/server'
 
-function page(){
-    return (
-        <div>page</div>
-    )
+connect()
+
+export async function POST (request: NextRequest) {
+    try {
+        const reqBody = request.json()
+        const {username, email , password } = reqBody
+        //validation 
+
+        console.log(reqBody);
+
+        const user = await User.findOne({email})
+
+        if(user){
+            request NextResponse.json ({ error : "User already 
+                exists"} ,{status : 400}
+            }
+            })
+    } catch (error: any) {
+        return NextResponse.json({error : error.message},
+            {status:500})
+    }
 }
-
-export default page
